525dd57bfcb8c927db8cb30cc352fcede88f3982
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-07-02 18:58:32 -0400

    Figured out the colspan

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,10 @@ $(document).ready(function() {
     // Create database reference
     var database = firebase.database();
 
+    // Creating nextArrival and minutesUntil as global variables to be used in .on("child_added") and in a separate interval function
+    var nextArrival;
+    var minutesUntil;
+
     // Create event listener for addTrain button
     $("#addTrain").on("click", function(event) {
         // Prevent Default submission and page refresh
@@ -51,7 +55,7 @@ $(document).ready(function() {
         console.log("frequency = " + frequency);
 
         // Calculate next arrival THIS WORKS AND RETURNS 17:25
-        var nextArrival = moment(firstTime, 'HH:mm').add(frequency, 'm').format("HH:mm");
+        nextArrival = moment(firstTime, 'HH:mm').add(frequency, 'm').format("HH:mm");
         console.log("nextArrival = " + nextArrival);
 
 
@@ -59,17 +63,21 @@ $(document).ready(function() {
         var currentTime = moment().format('HH:mm');
         var currentDate = moment().format('MM/DD/YYYY');
         console.log(currentDate);
+        // Format the current date and time together
         var nextFormatted = moment.utc(currentDate + ' ' + nextArrival).format("MM/DD/YYYY HH:mm");
         console.log("nextFormatted = " + nextFormatted);
-
-        var something = moment.duration(moment(nextFormatted).diff(moment()));
-        console.log("something = " + something);
-        console.log("something as minutes = " + something.asMinutes());
-        var minutesUntil = Math.floor(something.asMinutes());
+        // moment.duration of the formatted next arrival and its difference with the current moment in time.
+        var duration = moment.duration(moment(nextFormatted).diff(moment()));
+        console.log("duration = " + duration);
+        console.log("duration as minutes = " + duration.asMinutes());
+        minutesUntil = Math.floor(duration.asMinutes());
     
         // Append to the table
         $("#trainTable").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesUntil + "</td></tr>");
     });
+
+    // Update the minutesUntil and nextArrival time every minute
+
 });
 
 // var mins = moment.utc(moment(endTime, "HH:mm:ss").diff(moment(startTime, "HH:mm:ss"))).format("mm")
