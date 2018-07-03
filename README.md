f55c1caef39be352c38028b6d2e42dbd8f13e104
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-07-02 20:24:07 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TrainScheduler
+
+This is an app that allows users to add trains to a schedule that calculates and updates the next arrival time and how many minutes until next arrival.
+
+This app uses Firebase to store user input data, then manipulates that data to calculate the times needed. 
+
+User input for train names and destinations are simple strings, first arrival time is a time, and frequency is an integer. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,15 @@ $(document).ready(function() {
     // Creating nextArrival and minutesUntil as global variables to be used in .on("child_added") and in a separate interval function
     var nextArrival;
     var minutesUntil;
+    var trainName;
+    var destination;
+    var firstTime;
+    var frequency;
+    var currentTime;
+    var currentDate;
+    var nextFormatted;
+    var duration;
+    
 
     // Create event listener for addTrain button
     $("#addTrain").on("click", function(event) {
@@ -42,41 +51,51 @@ $(document).ready(function() {
         $("#frequency").val("");
     });
 
+    function calculateTime() {
+        // Format the current date and time together
+        nextFormatted = moment.utc(currentDate + ' ' + nextArrival).format("MM/DD/YYYY HH:mm");
+
+        // moment.duration of the formatted next arrival and its difference with the current moment in time.
+        // Calculate the difference between now and next arrival
+        duration = moment.duration(moment(nextFormatted).diff(moment()));
+
+        // Round down
+        minutesUntil = Math.floor(duration.asMinutes());
+    }
+
+    function appendTable() {
+        // Append to the table
+        $("#trainTable").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesUntil + "</td></tr>");
+    }
+
     // When the database is updated, add a new row containing the user input data to the table
     database.ref().on("child_added", function(snapshot) {
         // Get data from DB
-        var trainName = snapshot.val().name;
-        var destination = snapshot.val().destination;
-        var firstTime = snapshot.val().time;
-        var frequency = snapshot.val().frequency;
-
-        console.log("firstTime = " + firstTime);
-
-        console.log("frequency = " + frequency);
+        trainName = snapshot.val().name;
+        destination = snapshot.val().destination;
+        firstTime = snapshot.val().time;
+        frequency = snapshot.val().frequency;
 
         // Calculate next arrival THIS WORKS AND RETURNS 17:25
         nextArrival = moment(firstTime, 'HH:mm').add(frequency, 'm').format("HH:mm");
-        console.log("nextArrival = " + nextArrival);
-
 
         // Get current time THIS WORKS AS WELL
-        var currentTime = moment().format('HH:mm');
-        var currentDate = moment().format('MM/DD/YYYY');
-        console.log(currentDate);
-        // Format the current date and time together
-        var nextFormatted = moment.utc(currentDate + ' ' + nextArrival).format("MM/DD/YYYY HH:mm");
-        console.log("nextFormatted = " + nextFormatted);
-        // moment.duration of the formatted next arrival and its difference with the current moment in time.
-        var duration = moment.duration(moment(nextFormatted).diff(moment()));
-        console.log("duration = " + duration);
-        console.log("duration as minutes = " + duration.asMinutes());
-        minutesUntil = Math.floor(duration.asMinutes());
-    
-        // Append to the table
-        $("#trainTable").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesUntil + "</td></tr>");
+        currentTime = moment().format('HH:mm');
+        currentDate = moment().format('MM/DD/YYYY');
+
+        calculateTime();
+        appendTable();
     });
 
-    // Update the minutesUntil and nextArrival time every minute
+    // // Update the minutesUntil and nextArrival time every minute
+    // setTimeout(function() {
+    //     calculateTime()
+    //     if (minutesUntil <= 0) {
+    //         nextArrival = moment(nextArrival, 'HH:mm').add(frequency, 'm').format("HH:mm");
+    //         var table = $("#trainTable").html();
+    //         $("#trainTable").html(table);
+    //     }
+    // }, 3000);
 
 });
 
